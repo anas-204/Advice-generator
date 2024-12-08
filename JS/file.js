6f6febefda5fb901bc54a0156261ddb2f95a37f7
Anas Elsheikh <anaselsheikh2004@gmail.com>
2024-12-08 17:49:36 +0300

    advice number added

--- JS/file.js
+++ JS/file.js
@@ -1,8 +1,10 @@
 var advice = document.getElementById("advice");
+var adviceNum = document.getElementById("advice-num");
 var data = [];
 async function getIdea() {
   var response = await fetch("https://api.adviceslip.com/advice");
   data = await response.json();
   console.log(data);
   advice.innerHTML = data.slip.advice;
+  adviceNum.innerHTML = data.slip.id;
 }
